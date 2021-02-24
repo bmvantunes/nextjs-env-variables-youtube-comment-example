d8e9c8d1c96f847f42c774816d43e090580cfca8
Bruno Antunes <bmvantunes@gmail.com>
2021-02-24 23:28:16 +0000

    add getStaticProps

---
+++ pages/youtube-comment-getstaticprops.js
@@ -0,0 +1,24 @@
+export default function ({ serverPublicVar1, serverVar1 }) {
+    return (
+      <div>
+        <div>NEXT_PUBLIC_VAR1 = {process.env.NEXT_PUBLIC_VAR1}</div>
+        <div>FROM SERVER: NEXT_PUBLIC_VAR1 = {serverPublicVar1}</div>
+        ---------------------
+        <div>VAR1 = {process.env.VAR1 || '-----'}</div>
+        <div>FROM SERVER: VAR1 = {serverVar1}</div>
+      </div>
+    );
+  }
+  
+  export function getStaticProps() {
+    console.log('NODE.JS NEXT_PUBLIC_VAR1', process.env.NEXT_PUBLIC_VAR1);
+    console.log('NODE.JS VAR1', process.env.VAR1);
+  
+    return {
+      props: {
+        serverPublicVar1: process.env.NEXT_PUBLIC_VAR1,
+        serverVar1: process.env.VAR1,
+      },
+    };
+  }
+  
